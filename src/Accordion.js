c689591077ca963580b954760e271482519ebdbb
gabrielmussafiri <musgabriel2@gmail.com>
2023-07-07 10:53:09 +0200

    accordion updated

--- src/Accordion.js
+++ src/Accordion.js
@@ -4,7 +4,12 @@ export default function Accordion({ data }) {
   return (
     <div className="accordion">
       {data.map((el, index) => (
-        <AccordionItems title={el.title} text={el.text} num={index} />
+        <AccordionItems
+          title={el.title}
+          text={el.text}
+          num={index}
+          key={index}
+        />
       ))}
     </div>
   );
--- src/AccordionItems.js
+++ src/AccordionItems.js
@@ -1,11 +1,18 @@
+import { useState } from "react";
+
 export default function AccordionItems({ num, title, text }) {
-    
+  const [isOpen, setIsOpen] = useState(false);
+
+  function handleToggle() {
+    setIsOpen((isOpen) => !isOpen);
+  }
+
   return (
-    <div>
+    <div className="item" onClick={handleToggle}>
       <p className="number">{num < 9 ? `0${num + 1}` : num + 1}</p>
       <p className="title">{title}</p>
-      <p className="icon">-</p>
-      <div className="content-box">{text}</div>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
     </div>
   );
 }
